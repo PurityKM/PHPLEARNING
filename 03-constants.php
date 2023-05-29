2f25180e7366df6ad438b9879616624f2fc63d8d
PurityKM <irungu1210@gmail.com>
2023-05-29 15:30:40 +0300

    How to define a constant, it can only be written in CAPS etc.

---
+++ 03-constants.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>LEARNING PHP</title>
+    </head>
+    <body>
+    <?php
+        //Defining constants
+        //Constants are case sensitive and is written all in CAPS
+        define("title", "LEARNING PHP");
+        define("COLOR", "GREENYELLOW");
+
+        echo title;
+        echo COLOR;
+        ?>
+            
+    </body>
+</html>
