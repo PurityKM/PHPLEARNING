faa806674fb50fa98a1ae460a45556f32e23feb6
PurityKM <irungu1210@gmail.com>
2023-05-29 15:22:20 +0300

    Learning how to execute an output using print()function and echo function.

---
+++ 01-first.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>LEARNING PHP</title>
+    </head>
+    <body>
+        <?php echo '<h1>Hello World!</h1>';  ?>
+        <?php print '<h2>Hello Pals!</h2>'; ?>
+    </body>
+</html>
