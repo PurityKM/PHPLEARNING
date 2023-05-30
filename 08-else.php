44afc0174eb47822494799c85e1d91a79bde18a6
PurityKM <irungu1210@gmail.com>
2023-05-30 16:01:29 +0300

    else statement is used to execute code whenever if condition fails.

---
+++ 08-else.php
@@ -0,0 +1,17 @@
+<?php
+// if(expression){
+//     code to execute IF TRUE
+// } else{
+//     code to execute IF FALSE
+// }
+
+$fav_hobby = "Coding";
+
+
+if($fav_hobby == "Sleeping"){
+    echo "Sleeping causes laziness hence brokeness.";
+} else{
+    echo "<h1>I love and enjoy coding.</h1>";
+} 
+
+?>
