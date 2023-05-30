72c4d8c5dcfd925e3f3f1bbf5f0a778595315746
PurityKM <irungu1210@gmail.com>
2023-05-30 16:13:29 +0300

    String functions converts strings in whichever way/format you want.

---
+++ 13-string.php
@@ -0,0 +1,23 @@
+<?php
+
+//string operator
+$fname = 'Purity ';
+$sname = 'Muthoni ';
+
+?>
+
+<?php
+//concatenation operator
+$lname = $fname . $sname . 'Irungu';
+echo $lname;
+echo '<br>';
+
+//concatenating assignment operator
+$first = 'Maggie ';
+$sename ='Nyambura ';
+$funame = $first . $sename . 'Irungu';
+
+echo $funame;
+
+
+?>
