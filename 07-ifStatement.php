da3b95d700fb16bd5cb954190c5f6b91a3f9b89a
PurityKM <irungu1210@gmail.com>
2023-05-30 09:17:08 +0300

    Learning if statement which executes code if condition is true

---
+++ 07-ifStatement.php
@@ -0,0 +1,37 @@
+<?php
+
+
+//if (expression){
+    //code to be executed
+//}
+
+$a = 20;
+$b = 30;
+$c = 50;
+$d = 70;
+
+// echo $a;
+
+?>
+
+<html>
+    <body>
+    <?php    
+
+    if($a < $b){
+    //     print_r("$a is greater than $b");
+    // }
+    // else{
+    //     print_r("$a is less than $b");
+    // }
+        echo "$a is less than $b";
+    }
+echo '<br>';
+    if($d > $c){
+        echo "$d is greater than $c";
+    }
+    
+
+    ?>
+    </body>
+</html>
